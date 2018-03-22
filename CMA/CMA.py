79c44db195a2fb37873544e63d35cb13da75df02
Philipp Mueller <muellphi@ethz.ch>
2018-03-22 14:20:32 +0100

    add python files, delete cache from repo

---
+++ CMA/CMA.py
@@ -0,0 +1,18 @@
+# *
+# *  CMA_implementation.py
+# *  PyPi4U
+# *
+# *  Authors:
+# *     Paul Aurel Diederichs  - daurel@ethz.ch
+# *     Georgios Arampatzis - arampatzis@collegium.ethz.ch
+# *     Panagiotis Chatzidoukas
+# *  Copyright 2018 ETH Zurich. All rights reserved.
+# *
+
+import sys
+sys.path.insert(0, '../common')
+from read_in import read_in
+from optimize import CMA_method
+
+parameters = read_in()
+CMA_method(*parameters)
